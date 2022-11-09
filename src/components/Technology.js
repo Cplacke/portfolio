3e09dda71bcc14fe9704758b0f006c8ab3577615
cplacke <colin.m.placke@optum.com>
2022-11-09 12:34:35 -0500

    slider view for SM screen

--- src/components/Technology.js
+++ src/components/Technology.js
@@ -34,7 +34,7 @@ class Technology extends Component {
                 <h2 className="text-3xl text-center text-blue-300 mt-8"> Skills  &amp; Technologies </h2>  
 
                 <div className="m-auto mb-20 w-3/4 lg:w-3/5">
-                    <div className="my-6">
+                    <div className="mt-6">
                         <button className={this.jumpButtonClassList(this.state.activeType === 'Languages')}
                             onClick={() => { this.setState({ slideIndex: this.technology.findIndex((t) => (t.type === 'Languages'))/2, activeType: 'Languages'}) }} 
                         > Languages </button>
@@ -53,14 +53,26 @@ class Technology extends Component {
                             this.setState({ activeType: this.technology[next*2].type}) 
                         }}
                         slideIndex={this.state.slideIndex}
+                        renderTopRightControls={({ previousDisabled, nextSlide }) => (
+                            <button className="md:hidden uppercase text-blue-300 hover:text-white hover:bg-blue-300 border border-blue-300 rounded-full px-4 py-2"
+                                onClick={nextSlide} disabled={previousDisabled}>
+                                Next
+                            </button>
+                        )}
+                        renderTopLeftControls={({ previousDisabled, previousSlide }) => (
+                            <button className="md:hidden uppercase text-blue-300 hover:text-white hover:bg-blue-300 border border-blue-300 rounded-full px-4 py-2"
+                                onClick={previousSlide} disabled={previousDisabled}>
+                                Prev
+                            </button>
+                        )}
                         renderCenterLeftControls={({ previousDisabled, previousSlide }) => (
-                            <button className="absolute -left-20 uppercase text-blue-300 hover:text-white hover:bg-blue-300 border border-blue-300 rounded-full px-4 py-2"
+                            <button className="hidden md:block absolute -left-20 uppercase text-blue-300 hover:text-white hover:bg-blue-300 border border-blue-300 rounded-full px-4 py-2"
                                 onClick={previousSlide} disabled={previousDisabled}>
                                 Prev
                             </button>
                         )}
                         renderCenterRightControls={({ nextDisabled, nextSlide }) => (
-                            <button className="absolute -right-20 uppercase text-blue-300 hover:text-white hover:bg-blue-300 border border-blue-300 rounded-full px-4 py-2"
+                            <button className="hidden md:block absolute -right-20 uppercase text-blue-300 hover:text-white hover:bg-blue-300 border border-blue-300 rounded-full px-4 py-2"
                                 onClick={nextSlide} disabled={nextDisabled}>
                                 Next
                             </button>
@@ -71,7 +83,7 @@ class Technology extends Component {
                                 .map((item, i) => {
                                     if ( i%2 === 0 ) {
                                         // this.alt = !this.alt;
-                                        return <div key={`tech-${i}`}> {[this.technology[i], this.technology[i+1]].map((item, j) => {
+                                        return <div key={`tech-${i}`} className="mt-4"> {[this.technology[i], this.technology[i+1]].map((item, j) => {
                                             let classList = this.alt ? 'mr-auto ml-0' : 'ml-auto mr-0 text-right';
                                             this.alt = !this.alt;
                                             return (
