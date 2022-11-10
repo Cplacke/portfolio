eec9c76ce4f838958363bec4be3bc360740d5104
cplacke <colin.m.placke@optum.com>
2022-11-10 10:57:12 -0500

    handle app route refresh and redirects with servce static files

--- server.deno.ts
+++ server.deno.ts
@@ -1,9 +1,17 @@
 import { serve } from "https://deno.land/std@0.116.0/http/server.ts";
 import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts";
 
-const serveFiles = (req: Request) => staticFiles('build')({ 
-    request: req, 
-    respondWith: (r: Response) => r 
-})
+const serveFiles = (req) => {
+    if (/\/(home|projects|resume)/i.test(req.url)) {
+        console.info('APP PATH DETECTED: redirecting ...');
+        return Response.redirect(req.url.replace(/\/(home|projects|resume)/, '/'), 302);
+    }
+    return staticFiles('build')({ 
+        request: req,
+        respondWith: (r: Response) => r,
+    });
+}
 
-serve((req) => serveFiles(req), { addr: ':9000' });
+serve((req) => {
+    return serveFiles(req)
+}, { addr: ':9000' });
