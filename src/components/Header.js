681bc1ae3862c189417238c6ac99b4f28755a7c7
Kronos <kronos@kronoss-air.lan>
2025-10-22 10:40:29 -0400

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ class Header extends Component {
                 <h3 className="text-lg"> Full Stack Web Engineer </h3>
 
                 <div className="w-full px-4 md:p-0 md:w-3/4 lg:w-2/5 my-6 text-md text-gray-200 text-left m-auto">
-                    Welcome! I'm Colin Placke, a seasoned Senior Developer in the exciting and ever-evolving healthcare technology industry. Over the past 5+ years, I've been building large-scale multi-tenant applications for healthcare providers across the country at Optum Technology. As part of an agile and dynamic full-stack development team, I've become a go-to subject matter expert for front-end and JavaScript technologies. But I'm always eager for new challenges and opportunities to learn and grow. Let's connect and see how we can build something amazing together!
+                    Welcome! I'm Colin Placke, a seasoned Senior Developer in the exciting and ever-evolving healthcare technology industry. Over the past 7+ years, I've been building large-scale multi-tenant applications for healthcare providers across the country at Optum Technology. As part of an agile and dynamic full-stack development team, I've become a go-to subject matter expert for front-end and JavaScript technologies. But I'm always eager for new challenges and opportunities to learn and grow. Let's connect and see how we can build something amazing together!
                 </div>
                 <div className="w-full md:p-0 w-3/4 lg:w-2/5 my-6 text-md text-gray-200 text-left m-auto text-right font-fredoka">
                     "I'm a simple man trying to make my way through the galaxy." <span className="text-gray-400"> ~ Boba Fett </span>
